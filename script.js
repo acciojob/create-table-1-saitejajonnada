32d73bd6d54fff53305987d84e753dc9c5d68379
Jonnada Sai Teja <146433770+saitejajonnada@users.noreply.github.com>
2026-04-23 10:52:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 function insert_Row() {
   let table = document.getElementById("sampleTable");
 
-  // Get current number of rows
   let rowCount = table.rows.length;
 
-  // Create new row at the end
   let row = table.insertRow(rowCount);
 
-  // Insert cells
   let cell1 = row.insertCell(0);
   let cell2 = row.insertCell(1);
 
-  // Add text
-  cell1.innerHTML = "Row" + (rowCount + 1) + " cell1";
-  cell2.innerHTML = "Row" + (rowCount + 1) + " cell2";
+  // Fixed values as per requirement
+  cell1.innerHTML = "New Cell1";
+  cell2.innerHTML = "New Cell2";
 }
